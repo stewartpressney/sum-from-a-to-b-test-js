53450a2a47baf436a267e21eac65034a93bd80f7
stewartpressney <stewartpressney@gmail.com>
2018-03-28 18:58:39 -0700

    complete

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+
+  if (toN > fromN) {
+    return fromN + sum(fromN + 1, toN);
+  } else {
+  return fromN;
+  }
+ }
 
 module.exports = sum;
